1b319fba675b12e592f62a4bc6c518f34fb5256a
Kentaro Takiguchi <takiguchi0817@gmail.com>
2015-01-13 23:46:02 +0900

    Create RepositoryItemViewHolder

--- app/src/main/java/com/example/octodroid/RepositoryAdapter.java
+++ app/src/main/java/com/example/octodroid/RepositoryAdapter.java
@@ -1,24 +1,19 @@
 package com.example.octodroid;
 
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.rejasupotaro.octodroid.models.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-
 public class RepositoryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private static class ViewType {
         private static final int ITEM = 1;
         private static final int FOOTER = 2;
     }
+
     private List<Repository> repositories = new ArrayList<>();
 
     public RepositoryAdapter(RecyclerView recyclerView, RecyclerView.LayoutManager layoutManager) {
@@ -73,24 +68,5 @@ public class RepositoryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
     public void addRepositories(List<Repository> repositories) {
         this.repositories.addAll(repositories);
         notifyDataSetChanged();
-    }
-
-    public static class RepositoryItemViewHolder extends RecyclerView.ViewHolder {
-        @InjectView(R.id.name)
-        TextView nameTextView;
-
-        public static RepositoryItemViewHolder create(ViewGroup parent) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_repository, parent, false);
-            return new RepositoryItemViewHolder(view);
-        }
-
-        public RepositoryItemViewHolder(View view) {
-            super(view);
-            ButterKnife.inject(this, view);
-        }
-
-        private void bind(final Repository repository) {
-            nameTextView.setText(repository.getName());
-        }
     }
 }
---
+++ app/src/main/java/com/example/octodroid/RepositoryItemViewHolder.java
@@ -0,0 +1,32 @@
+package com.example.octodroid;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.rejasupotaro.octodroid.models.Repository;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class RepositoryItemViewHolder extends RecyclerView.ViewHolder {
+    @InjectView(R.id.name)
+    TextView nameTextView;
+
+    public static RepositoryItemViewHolder create(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_repository, parent, false);
+        return new RepositoryItemViewHolder(view);
+    }
+
+    public RepositoryItemViewHolder(View view) {
+        super(view);
+        ButterKnife.inject(this, view);
+    }
+
+    public void bind(final Repository repository) {
+        nameTextView.setText(repository.getName());
+    }
+}
+
