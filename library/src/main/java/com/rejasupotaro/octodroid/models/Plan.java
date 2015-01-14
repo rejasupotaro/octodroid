de8904e596d976465bd07070de52c5937b69d61b
Kentaro Takiguchi <takiguchi0817@gmail.com>
2015-01-14 23:55:31 +0900

    Add annotations

--- library/src/main/java/com/rejasupotaro/octodroid/models/Plan.java
+++ library/src/main/java/com/rejasupotaro/octodroid/models/Plan.java
@@ -1,8 +1,14 @@
 package com.rejasupotaro.octodroid.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Plan {
+    @SerializedName("name")
     private String name;
+    @SerializedName("space")
     private int space;
+    @SerializedName("private_repos")
     private int privateRepos;
+    @SerializedName("collaborators")
     private int collaborators;
 }
