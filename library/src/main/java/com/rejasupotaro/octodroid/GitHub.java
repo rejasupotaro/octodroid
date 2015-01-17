35305736d66152b71bc8808dd342636b22ec3862
Kentaro Takiguchi <takiguchi0817@gmail.com>
2015-01-17 16:03:36 +0900

    Better locking

--- library/src/main/java/com/rejasupotaro/octodroid/GitHub.java
+++ library/src/main/java/com/rejasupotaro/octodroid/GitHub.java
@@ -6,9 +6,13 @@ import com.rejasupotaro.octodroid.http.ApiClient;
 public class GitHub {
     private static GitHubClient client;
 
-    public static synchronized GitHubClient client() {
+    public static GitHubClient client() {
         if (client == null) {
-            client = new GitHubClient(new ApiClient());
+            synchronized (GitHub.class) {
+                if (client == null) {
+                    client = new GitHubClient(new ApiClient());
+                }
+            }
         }
         return client;
     }
