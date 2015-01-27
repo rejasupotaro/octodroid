2a9eba8fcdf24da8d2098c0c4a2e9b10580127c9
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2015-01-27 17:50:47 +0700

    Enable to print formatted json

--- library/src/main/java/com/rejasupotaro/octodroid/GsonProvider.java
+++ library/src/main/java/com/rejasupotaro/octodroid/GsonProvider.java
@@ -1,9 +1,10 @@
 package com.rejasupotaro.octodroid;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class GsonProvider {
-    private static Gson gson = new Gson();
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public static void set(Gson gson) {
         GsonProvider.gson = gson;
