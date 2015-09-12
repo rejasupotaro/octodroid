46ebbd3eab62e1349e86c33cac0b1a5a1339eaf7
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2015-09-12 17:43:10 +0900

    Output request logs only if BuildConfig.DEBUG

--- library/src/main/java/com/rejasupotaro/octodroid/http/interceptors/LoggingInterceptor.java
+++ library/src/main/java/com/rejasupotaro/octodroid/http/interceptors/LoggingInterceptor.java
@@ -2,6 +2,7 @@ package com.rejasupotaro.octodroid.http.interceptors;
 
 import android.util.Log;
 
+import com.rejasupotaro.octodroid.BuildConfig;
 import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -15,19 +16,25 @@ public class LoggingInterceptor implements Interceptor {
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
 
-        long t1 = System.nanoTime();
-        Log.i(TAG, String.format("Sending request %s on %s%n%s",
-                request.url(),
-                chain.connection(),
-                request.headers()));
+        long t1 = 0, t2;
+
+        if (!BuildConfig.DEBUG) {
+            t1 = System.nanoTime();
+            Log.i(TAG, String.format("Sending request %s on %s%n%s",
+                    request.url(),
+                    chain.connection(),
+                    request.headers()));
+        }
 
         Response response = chain.proceed(request);
 
-        long t2 = System.nanoTime();
-        Log.i(TAG, String.format("Received response for %s in %.1fms%n%s",
-                response.request().url(),
-                (t2 - t1) / 1e6d,
-                response.headers()));
+        if (!BuildConfig.DEBUG) {
+            t2 = System.nanoTime();
+            Log.i(TAG, String.format("Received response for %s in %.1fms%n%s",
+                    response.request().url(),
+                    (t2 - t1) / 1e6d,
+                    response.headers()));
+        }
 
         return response;
     }
