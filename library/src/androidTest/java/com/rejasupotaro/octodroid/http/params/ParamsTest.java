2d2644dc22736c585c290c397a012f76eeaaea21
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2015-09-13 18:30:36 +0900

    Create Params class

---
+++ library/src/androidTest/java/com/rejasupotaro/octodroid/http/params/ParamsTest.java
@@ -0,0 +1,44 @@
+package com.rejasupotaro.octodroid.http.params;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+public class ParamsTest {
+
+    @Test
+    public void buildEmptyQuery() {
+        Params params = new Params();
+        assertThat(params.toString()).isEqualTo("page=1");
+    }
+
+    @Test
+    public void buildQuery() {
+        {
+            Params params = new Params()
+                    .add("q", "Android Java");
+            assertThat(params.toString()).isEqualTo("q=Android+Java&page=1");
+        }
+        {
+            Params params = new Params()
+                    .add("order", "desc")
+                    .add("all", "true")
+                    .add("sort", "stars")
+                    .add("type", "owner");
+            assertThat(params.toString()).isEqualTo("all=true&order=desc&sort=stars&type=owner&page=1");
+        }
+    }
+
+    @Test
+    public void incrementPage() {
+        Params params = new Params();
+        assertThat(params.toString()).isEqualTo("page=1");
+        params.incrementPage();
+        assertThat(params.toString()).isEqualTo("page=2");
+    }
+}
---
+++ library/src/main/java/com/rejasupotaro/octodroid/http/params/Params.java
@@ -0,0 +1,42 @@
+package com.rejasupotaro.octodroid.http.params;
+
+import android.text.TextUtils;
+
+import com.rejasupotaro.octodroid.utils.UrlUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Params {
+
+    private Map<String, String> queryMap;
+
+    public Params() {
+        queryMap = new HashMap<>();
+        queryMap.put("page", "1");
+    }
+
+    public Params add(String key, String value) {
+        queryMap.put(key, value);
+        return this;
+    }
+
+    public void incrementPage() {
+        int page = Integer.parseInt(queryMap.get("page"));
+        queryMap.put("page", String.valueOf(++page));
+    }
+
+    @Override
+    public String toString() {
+        List<String> queryList = new ArrayList<>();
+        for (String key : queryMap.keySet()) {
+            String value = queryMap.get(key);
+            String query = String.format("%s=%s", key, UrlUtils.encode(value));
+            queryList.add(query);
+        }
+
+        return TextUtils.join("&", queryList);
+    }
+}
