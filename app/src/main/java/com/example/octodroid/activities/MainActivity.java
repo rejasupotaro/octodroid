6d27382c3a275bfc7113c5285b3f31e98174a626
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2015-10-06 19:40:23 +0900

    Delete drawer from MainActivity

--- app/src/main/java/com/example/octodroid/activities/MainActivity.java
+++ app/src/main/java/com/example/octodroid/activities/MainActivity.java
@@ -2,38 +2,23 @@ package com.example.octodroid.activities;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.octodroid.R;
-import com.example.octodroid.data.GitHub;
 import com.example.octodroid.data.SessionPrefs;
 import com.example.octodroid.data.SessionPrefsSchema;
 import com.example.octodroid.views.adapters.HottestRepositoryAdapter;
-import com.example.octodroid.views.components.ProfileView;
-import com.rejasupotaro.octodroid.http.Response;
-import com.rejasupotaro.octodroid.models.User;
-import com.trello.rxlifecycle.ActivityEvent;
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends RxAppCompatActivity {
-    @Bind(R.id.drawer_layout)
-    DrawerLayout drawerLayout;
-    @Bind(R.id.navigation_drawer)
-    ProfileView navigationDrawerView;
     @Bind(R.id.hottest_repository_list)
     RecyclerView hottestRepositoryListView;
-
-    private ActionBarDrawerToggle drawerToggle;
 
     private HottestRepositoryAdapter hottestRepositoryAdapter;
 
@@ -76,11 +61,6 @@ public class MainActivity extends RxAppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                } else {
-                    drawerLayout.openDrawer(GravityCompat.START);
-                }
                 return true;
             case R.id.action_search:
                 SearchResultActivity.launch(this);
@@ -90,53 +70,12 @@ public class MainActivity extends RxAppCompatActivity {
         }
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        if (drawerToggle != null) {
-            drawerToggle.syncState();
-        }
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        if (drawerToggle != null) {
-            drawerToggle.onConfigurationChanged(newConfig);
-        }
-    }
-
     public void setupActionBar() {
         getSupportActionBar().setTitle("");
     }
 
     private void setupViews() {
-        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.app_name, R.string.app_name);
-        drawerToggle.setDrawerIndicatorEnabled(true);
-        drawerLayout.setDrawerListener(drawerToggle);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-        navigationDrawerView.setup();
-
         hottestRepositoryAdapter = new HottestRepositoryAdapter(hottestRepositoryListView);
         hottestRepositoryListView.setAdapter(hottestRepositoryAdapter);
-
-        requestProfile();
-    }
-
-    private void requestProfile() {
-        SessionPrefs prefs = SessionPrefsSchema.get(this);
-        GitHub.client().authorization(prefs.getUsername(), prefs.getPassword());
-        GitHub.client().user()
-                .cache()
-                .compose(this.<Response<User>>bindUntilEvent(ActivityEvent.DESTROY))
-                .subscribe(r -> {
-                    if (!r.isSuccessful()) {
-                        return;
-                    }
-                    navigationDrawerView.setUser(r.entity());
-                });
     }
 }
